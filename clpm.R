985ade28af9aa4c88d54b5cfd0831fdf42e06824
rich <rlucas11@yahoo.com>
2023-02-03 08:35:21 -0500

    Updated clpm.R file with buildModel

--- clpm.R
+++ clpm.R
@@ -5,4 +5,42 @@ library(haven)
 library(psych)
 library(ggplot2)
 
-data <- read_csv("nhatsCleaned_new.csv")
+## Read utilities
+source("~/Projects/starts/scripts/usefulFunctions.R")
+
+## Read script to build models
+source("~/Projects/code-generator/buildModel.R")
+
+## Read data
+## data <- read_csv("nhatsCleaned_new.csv")
+data <- read_csv("data/nhatsCleaned.csv")
+
+
+## Get Data
+dataSelect <- data %>%
+  select(starts_with("PositiveEmotion"),
+         starts_with("health"))
+names(dataSelect) <- c(paste0("x", 1:11),
+                       paste0("y", 1:11))
+
+
+test <- buildStarts2(11)
+testFit <- lavaan(test, data=dataSelect, estimator = "MLR", missing="fiml")
+summary(testFit)
+
+
+riclpm <- buildRiclpm(11)
+riclpmFit <- lavaan(ricplm, data=dataSelect, estimator = "MLR", missing="fiml")
+summary(riclpmFit)
+
+
+## Model with no trait variance
+test <- buildModel(11, trait=FALSE, stationarity=TRUE)
+testFit <- lavaan(test, data=dataSelect, estimator = "MLR", missing="fiml")
+summary(testFit, fit.measures=TRUE)
+
+## Regular CLPM
+clpm <- buildClpm(11)
+clpmFit <- lavaan(clpm, data = dataSelect, estimator = "MLR", missing = "fiml")
+summary(clpmFit, fit.measures = TRUE)
+
